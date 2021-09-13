7f70bac22c189aa1a88466685092d05c8491e5e5
Alexey Scripkin <a.scripkin@gmail.com>
2021-09-13 13:47:45 +0300

    using scala 3

--- src/SqliteScala.scala
+++ src/SqliteScala.scala
@@ -7,23 +7,23 @@ import java.sql._
 
 object SqliteScala  extends App {
 
-  implicit class TypeResultSet(rs: ResultSet) extends Iterator[ClientType] {
+    implicit class TypeResultSet(rs: ResultSet) extends Iterator[ClientType] {
+        extension (rs: ResultSet)
+            def getString(name: String, encoding: String) = String(rs.getBytes(name),encoding)
 
-    def getString(name: String, encoding: String) = new String(rs.getBytes(name),encoding)
+        override def hasNext: Boolean = rs.next
+        override def next: ClientType = ClientType(rs.getInt("type"), rs.getString("name", "Windows-1251"))
+    }
 
-    override def hasNext: Boolean = rs.next
-    override def next: ClientType = ClientType(rs.getInt("type"), rs.getString("name", "Windows-1251"))
-  }
+    case class ClientType(tCode: Int, name: String) {
+        override def toString: String = "--%3d\t%-20s".format(tCode, name)
+    }
 
-  case class ClientType(tCode: Int, name: String) {
-    override def toString: String = "--%3d\t%-20s".format(tCode, name)
-  }
+    Class.forName("org.sqlite.JDBC")
 
-  Class.forName("org.sqlite.JDBC")
+    lazy val conn: Connection = DriverManager.getConnection("jdbc:sqlite:data/proc_new.db")
+    lazy val stmt: Statement = conn.createStatement
+    lazy val rs:   ResultSet = stmt.executeQuery("SELECT type, name FROM type")
 
-  lazy val conn: Connection = DriverManager.getConnection("jdbc:sqlite:data/proc_new.db")
-  lazy val stmt: Statement = conn.createStatement
-  lazy val rs:   ResultSet = stmt.executeQuery("SELECT type, name FROM type")
-
-  rs foreach println
+    rs foreach println
 }
